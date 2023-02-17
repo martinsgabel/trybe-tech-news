569de13a7f684a198e271db2117a20d46d058ec9
martins <gabrielamspinola@gmail.com>
2023-02-17 00:54:04 -0300

    [feat]: url requests, news link scrap and next page link

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,16 +1,43 @@
 # Requisito 1
+from parsel import Selector
+import requests
+from requests.exceptions import ConnectTimeout, HTTPError
+import time
+
+
 def fetch(url):
-    """Seu código deve vir aqui"""
+    time.sleep(1)
+    try:
+        response = requests.get(
+            url, headers={"user-agent": "Fake user-agent"}, timeout=3
+        )
+        response.raise_for_status()
+    except (ConnectTimeout, HTTPError):
+        return None
+
+    return response.text
 
 
 # Requisito 2
 def scrape_updates(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(html_content)
+    news_urls = []
+    for news in selector.css("main > div > div > div"):
+        link = news.css(
+            "article > div > div:nth-child(2) > header > h2 > a ::attr(href)"
+        ).get()
+        news_urls.append(link)
+    return news_urls
 
 
 # Requisito 3
 def scrape_next_page_link(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(html_content)
+    try:
+        next_page = selector.css("a.next.page-numbers ::attr(href)").get()
+    except requests.RequestException:
+        return None
+    return next_page
 
 
 # Requisito 4
